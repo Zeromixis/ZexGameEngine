813ee034df0cbb955a3a7730898720458398a0df
zeromixis <zeromixis@gmail.com>
2015-05-27 21:10:34 +0800

    *: Add Observer pattern to test

--- ZexGameEngine/include/Pattern/Observer.h
+++ ZexGameEngine/include/Pattern/Observer.h
@@ -1,9 +1,45 @@
 #ifndef _PATTERN_OBSERVER_H_
 #define _PATTERN_OBSERVER_H_
 
+#include <set>
+
 namespace ZGE
 {
+	template < typename... Args >
+	class Observer;
 
+	template < typename... Args >
+	class Subject
+	{
+	public:
+		void AddObserver ( Observer< Args... > &ob )
+		{
+			m_ObserverList.insert ( &ob );
+		}
+
+		void RemoveObserver ( Observer< Args... > &ob )
+		{
+			m_ObserverList.erase ( &ob );
+		}
+
+		void NotifyAll ( Args... args )
+		{
+			for ( auto &ob : m_ObserverList )
+			{
+				ob->OnNotify ( this, args... );
+			}
+		}
+
+	private:
+		std::set < Observer * > m_ObserverList;
+	};
+
+	template < typename... Args >
+	class Observer
+	{
+	public:
+		virtual void OnNotify ( Subject< typename Args... > &subject, Args... args) = 0;
+	};
 }
 
 #endif // _PATTERN_OBSERVER_H_
